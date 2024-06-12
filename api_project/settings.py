4302f02922065284e9b33b92124c3ee5d874a80a
uraniumkid30 <uraniumkid30@gmail.com>
2024-06-12 10:41:00 +0100

    api files

--- api_project/settings.py
+++ api_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY',"783g4hwg747wfvb7f42v7wvbbve7")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_SETTINGS', 'False').lower() in ('true', '1', 't')
